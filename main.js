68d96c06bfe02c561b8a1276b197e9d552cc7ae6
YousefSaeed668 <137455394+YousefSaeed668@users.noreply.github.com>
2023-08-06 13:23:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector(".img img")
+let all = document.querySelector(".all")
+function phones (src){
+  img.src = src;
+}
+
+function colors (color){
+  all.style.backgroundColor = color;
+};
